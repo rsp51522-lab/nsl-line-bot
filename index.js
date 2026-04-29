e7669cf95935f55f8306c0a5fa2e5608cedd8c06
rsp51522-lab <rsp51522@gmail.com>
2026-04-29 23:09:35 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ import axios from "axios";
 const app = express();
 app.use(express.json());
 
-const LINE_TOKEN = "OqLHoETrRgu0+fZtpgBQyMG7jhujFR4o3XZCc7u2mnvERueWQ+hjITWadnoyg3cFStWw3z7MNLnWTWkOZPH/TlUOzq9Q5zrLgBSN/pmzc+Vu8sTg+gIla0QxDymcVyDRPRMeccTMkoXcX8tZ/naVbgdB04t89/1O/w1cDnyilFU=";
-const OPENAI_API_KEY = "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";
+const LINE_TOKEN = process.env.LINE_TOKEN;
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 
 app.post("/webhook", async (req, res) => {
   res.sendStatus(200);
